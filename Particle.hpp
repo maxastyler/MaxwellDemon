75835057ef561bcb943ad17f1a64f13ca1ef866c
Max Tyler <mtyler88@gmail.com>
2017-02-15 11:40:52 +0000

    Fill out more functions in particle.cpp

--- Particle.hpp
+++ Particle.hpp
@@ -5,15 +5,19 @@
 
 class Particle
 {
+  Vec2 vel;
 public:
   Vec2 pos;
-  Vec2 vel;
   float radius;
   float mass;
+  float energy;
 
-  Particle(Vec2 _pos, Vec2 _vel, float _radius, float _mass):pos(_pos), vel(_vel), radius(_radius), mass(_mass){};
-  Particle(float _x, float _y, float _vx, float _vy, float _radius, float _mass):pos(Vec2(_x, _y)), vel(Vec2(_vx, _vy)), radius(_radius), mass(_mass){};
-  float getEnergy(){return (1/2)*mass*vel.lengthSquare();};
+  Particle(Vec2 _pos, Vec2 _vel, float _radius, float _mass):pos(_pos), radius(_radius), mass(_mass){setVel(_vel);};
+  Particle(float _x, float _y, float _vx, float _vy, float _radius, float _mass):pos(Vec2(_x, _y)), radius(_radius), mass(_mass){setVel(_vx, _vy);};
+  void updateEnergy(){energy=(1/2)*mass*vel.lengthSquare();};
+  void setVel(float _x, float _y){vel.x=_x, vel.y=_y; updateEnergy();};
+  void setVel(Vec2 _vel){vel=_vel; updateEnergy();};
+  Vec2 getVel(){return vel;};
 };
 
 #endif
