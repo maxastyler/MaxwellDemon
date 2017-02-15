9914002cd31b707efb0b125617fd1a082970aad0
Max Tyler <mtyler88@gmail.com>
2017-02-15 11:51:39 +0000

    make particle store the velocity squared, for less unnecessary calculations

--- Particle.hpp
+++ Particle.hpp
@@ -2,12 +2,15 @@
 #define PARTICLE
 
 #include "Vector.hpp"
+#include <math.h>
 
 class Particle
 {
   Vec2 vel;
 public:
   Vec2 pos;
+  float velSq;
+  float velMag;
   float radius;
   float mass;
   float energy;
@@ -16,7 +19,7 @@ public:
   Particle(float _x, float _y, float _vx, float _vy, float _radius, float _mass):pos(Vec2(_x, _y)), radius(_radius), mass(_mass){setVel(_vx, _vy);};
   void updateEnergy(){energy=(1/2)*mass*vel.lengthSquare();};
   void setVel(float _x, float _y){vel.x=_x, vel.y=_y; updateEnergy();};
-  void setVel(Vec2 _vel){vel=_vel; updateEnergy();};
+  void setVel(Vec2 _vel){vel=_vel; updateEnergy(); velSq=vel.lengthSquare(); velMag=std::sqrt(velSq);};
   Vec2 getVel(){return vel;};
 };
 
